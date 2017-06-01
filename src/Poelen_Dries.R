747ef4ae417a8f45f03edb79479d9629a157f9ac
ToonVanDaele <toon.vandaele@inbo.be>
2017-06-01 15:20:48 +0200

    Return all conventionele poelen (instead of only the top10). Added plots to illustrate the differences between the distance measures.

--- src/Poelen_Dries.R
+++ src/Poelen_Dries.R
@@ -22,23 +22,32 @@ lgbspr <- lgb %>%
   tidyr::spread(LGB_ORCA, buff_pct, fill = 0) %>%   # voor elk landgebruik een kolom met de oppervlakte (%)
   dplyr::select(-overig)                            # de variabele 'overig' laten vallen (komt weinig voor)
 
+#lgbspr <- dplyr::select(lgbspr, BW_ID, akker, cultuurgrasland) # Als check, de berekening met slecht 2 variabelen
+
+# Landgebruik als matrix
+m_lgbspr <- lgbspr %>%
+  dplyr::select(-BW_ID) %>%
+  as.matrix()
+
+# Covariantiematrix voor de landgebruiken van de poelen
+cov_lgb <- lgbspr %>%
+  dplyr::select(-BW_ID) %>%
+  as.matrix() %>%
+  cov()
+
+# Vector met de ID's van de bio poelen
+v_poelbio <- as.character(poelbio[,"BW_ID"])
 
 # Een functie die voor elke biopoel, de 10 meest nabije (gelijkaardige)
 # conventionele poelen teruggeeft
 getSimilarLandUse <- function(mypoel = "BW_40557") {
 
-  # Maak een vector met het langebruik van de biopoel
-  lgb_mypoel <- dplyr::filter(lgbspr, BW_ID == mypoel)
-  v_lgb_mypoel <- as.numeric(lgb_mypoel[1,-c(1)])
-  
-  # Selectie van de conventionele poelen nabij (< 3km) de biopoel
-  conv_sel <- dplyr::filter(poeldist, BW_ID == mypoel)
-  conIDs <- conv_sel[,"BW_ID_ConPoel"]
-  
-  # Maak een matrix met het landgebruik van de conventionele poelen
-  lgb_conv <- dplyr::filter(lgbspr, BW_ID %in% conIDs)
-  mat_lgb_conv <- as.matrix(lgb_conv[,-c(1)])
-  
+  # Een vector met het langebruik van de biopoel
+  v_lgb_mypoel <- lgbspr %>%
+    dplyr::filter(BW_ID == mypoel) %>%  # Selecteer de rij met 'mypoel'
+    dplyr::select(-BW_ID) %>%           # Selecteer alle kolommen behalve BW_ID
+    as.numeric()                        # in vector formaat
+
   # Bereken afstanden
   d_maha <- mahalanobis(m_lgbspr, v_lgb_mypoel, cov_lgb)
   d_norm <- mahalanobis(m_lgbspr, v_lgb_mypoel, diag(diag(cov_lgb)))
@@ -54,32 +63,81 @@ getSimilarLandUse <- function(mypoel = "BW_40557") {
     dplyr::select(BW_ID_ConPoel) %>%
     .[["BW_ID_ConPoel"]]
   
-  # Sorteren van laag naar hoog en de 10 eerst 10 records nemen
-  df_dist_top10 <- df_dist %>%
-    dplyr::arrange(dist) %>%
-    dplyr::slice(1:10)
+  # Sorteren van laag naar hoog
+  df <- df %>%
+    dplyr::filter(BW_ID %in% conv_sel) %>%  # Selecteer enkel conv poelen in omgeving
+    dplyr::arrange(d_maha)              # Sorteren volgens afstandsmaat mahalanobis
+
   # Ranking berekenen voor elke afstandsmaat
   df$r_maha <- rank(df$d_maha)
   df$r_norm <- rank(df$d_norm)
   df$r_eucl <- rank(df$d_eucl)
   
-  return(df_dist_top10)
+  # Toevoegen van de geografische afstand tussen de biopoel en conventionele poelen
+  df <- df %>%
+    dplyr::left_join(poeldist, by = c("bio_ID" = "BW_ID", "BW_ID" = "BW_ID_ConPoel"),) %>%   # geografische afstand
+    dplyr::left_join(lgbspr, by = "BW_ID")     # landgebruik conventionele poelen 
+
+  return(df)
 }  
 
-# Vector met bio poelen
-v_poelbio <- as.character(poelbio[,"BW_ID"])
 
-# Lege  data frame
-output <- data.frame()
+output <- data.frame()  # Lege  data frame
 
 # Loop door alle biopoelen, voer de functie getSimilarLanduse uit
-# en voeg de resultaten onderaan toe in de data frame output.
+# en voeg de resultaten aan de data frame 'output'.
 for (mypoel in v_poelbio) {
   
-  top10 <- getSimilarLandUse(mypoel)
-  output <- rbind(output, top10)
+  df <- getSimilarLandUse(mypoel)
+  output <- rbind(output, df)
 }
 
 head(output)
 
-write.csv(x = output, file = "Biopoel_top10.csv")
+write.csv(x = output, file = "Biopoel_all.csv")
+
+output_top10 <- output %>%
+  dplyr::group_by(bio_ID) %>%             # groepeer op bio_ID
+  dplyr::top_n(-10, r_maha)                # Selecteer enkel de top10
+
+write.csv(x = output_top10, file = "Biopoel_top10.csv")
+
+
+# Enkele regels code om het verschil tussen de drie afstandsmaten
+# te illlustreren.
+#
+# De plot geeft slechts twee assen weer (akker & cultuurgrasland)
+#
+# Omwille van de indormatie uit de andere assen kan de ranking soms
+# verrassend overkomen.
+#
+# Om de berekening te illustreren met slecht twee variabelen 
+# kan je regel 25 toevoegen:
+# "lgbspr <- dplyr::select(lgbspr, BW_ID, akker, cultuurgrasland)"
+
+
+library(ggplot2)
+
+mypoel <- "BW_40557"    # eender welke ID van een biopoel
+
+df_plot <- output %>%
+  dplyr::filter(bio_ID == mypoel) %>%
+  dplyr::top_n(-20, r_maha)
+df_bio <- dplyr::filter(lgbspr, BW_ID == mypoel)
+
+ggplot(df_plot, aes(x = akker, y = cultuurgrasland)) + geom_point() +
+  geom_text(aes(label = r_maha), hjust = 2, vjust = 0) +
+  geom_point(aes(x = df_bio$akker, y = df_bio$cultuurgrasland), color = "red") +
+  coord_fixed()
+  
+ggplot(df_plot, aes(x = akker, y = cultuurgrasland)) + geom_point() +
+  geom_text(aes(label = r_eucl), hjust = 2, vjust = 0) +
+  geom_point(aes(x = df_bio$akker, y = df_bio$cultuurgrasland), color = "red") +
+  coord_fixed()
+
+ggplot(df_plot, aes(x = akker, y = cultuurgrasland)) + geom_point() +
+  geom_text(aes(label = r_norm), hjust = 2, vjust = 0) +
+  geom_point(aes(x = df_bio$akker, y = df_bio$cultuurgrasland), color = "red") +
+  coord_fixed()
+
+
